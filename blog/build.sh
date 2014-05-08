829cc6933aa34310fb230e10c178bc41eea04a56
pigoneand <sjtu.pigoneand@gmail.com>
2014-05-07 18:26:04 -0700

    ok

--- blog/build.sh
+++ blog/build.sh
@@ -1,4 +1,4 @@
 ~/node_modules/hexo/bin/hexo migrate image
 ~/node_modules/hexo/bin/hexo generate
 ~/node_modules/hexo/bin/hexo deploy
-~/node_modules/hexo/bin/hexo server
+#~/node_modules/hexo/bin/hexo server
