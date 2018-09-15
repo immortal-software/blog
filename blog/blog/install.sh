ca97115ff06d97fe7c0e5d9a93d7104101464c86
Yihe Zhu <yihe.zhu@yitu-inc.com>
2018-09-15 20:03:54 +0800

    add new blog index

---
+++ blog/blog/install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/wzpan/hexo-theme-freemind.git themes/freemind
+npm install hexo --save
+npm install hexo-cli --save
+npm install hexo-deployer-git --save
+npm install hexo-server --save
